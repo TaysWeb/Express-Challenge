2fe2582a3d06ff86fe512033e68e9684ecdb7286
TaysWeb <snelltararobin@gmail.com>
2023-08-11 16:19:29 +0200

    added the database.js

--- api/config/index.js
+++ api/config/index.js
@@ -1,1 +1,32 @@
+const express = require('express')
+const app = express();
+const port = process.env.PORT || 8000 ;
 
+const router = require("../config/")
+
+require('dotenv').config();
+
+
+//middleware
+app.use(express.json()) ;
+app.use(cors());
+
+app.get( '/', (req, res) => {
+    res.sendFile('index.html');
+});
+app.get( '/', (req, res) => {
+    res.sendFile('<html><body><h4>hi there little human! </h4></body></html>');
+})
+//route
+
+app.get('/users', (req,res) => {
+        res.send(users);
+});
+
+
+//route
+app.use("/", router);
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
