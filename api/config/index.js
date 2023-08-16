000db5409a877ea20f3a33b20fa51e7747614a9a
TaysWeb <snelltararobin@gmail.com>
2023-08-16 10:00:09 +0200

    changes to css file

--- api/config/index.js
+++ api/config/index.js
@@ -1,12 +1,14 @@
 // only the db has to be configured
- const mysql = require('mysql');
+
+const mysql = require('mysql');
 
 const myDB = mysql.createPool({
     port : process.env.DB_PORT,
     host: process.env.DB_HOST ,
     user : process.env.DB_USER,
     password : process.env.DB_PASS, 
-    database : process.env.DB_NAME
+    database : process.env.DB_NAME,
+    connectionLimit : 10
 });
 
 myDB.getConnection(function(err){
